c6cf6f27aa5e8ba6e9419ecb835a90d04dd354ca
youxunzhang <youxun2023@gmail.com>
2026-07-23 07:48:35 +0800

    replace homepage with biography platform

---
+++ _next/static/aLZtQXJ-zpjne74NHWWKW/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fbooks\u002F[slug]","\u002Fgo\u002F[slug]","\u002Fpeople\u002F[slug]","\u002Ftopics\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
